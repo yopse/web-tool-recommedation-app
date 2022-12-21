e41692025174e0aa0e3a39590f09ff0518ebbe25
yopse <82827725+yopse@users.noreply.github.com>
2022-12-21 15:46:47 +0530

    all things fixed fontSize enhenced

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const buttonList = ["code", "docs", "webdev", "devops"];
+
+const toolsBucket = {
+  code: {
+    tools: ["VSCode", "Atom", "Sublime", "Notepad++"],
+    ratings: ["5/5", "4/5", "3.5/5", "3/5"],
+  },
+
+  docs: {
+    tools: ["MDN", "stackoverflow", "W3Schools"],
+    ratings: ["5/5", "4.5/5", "4/5"],
+  },
+
+  webdev: {
+    tools: ["Chrome devtools", "codesandbox", "codepen", "Netlify"],
+    ratings: ["5/5", "4.5/5", "4/5", "4/5"],
+  },
+
+  devops: {
+    tools: ["Git", "Docker", "kubernetes"],
+    ratings: ["5/5", "4.5/5", "4/5"],
+  },
+};
+
+export default function App() {
+  const [toolName, setToolName] = useState("webdev");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        devTools
+        <p>some recommended developer tools</p>
+      </div>
+
+      <main>
+        <div className="leftPanel">
+          <p>Click to get started</p>
+
+          {buttonList.map((item, index) => {
+            return (
+              <button
+                className="leftPanelButton"
+                key={index}
+                onClick={() => setToolName(item)}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+
+        <div className="rightPanel">
+          <p>Recommendations: </p>
+          <ul>
+            {toolsBucket[toolName].tools.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item}
+                  <span> {toolsBucket[toolName].ratings[index]} </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
-
-export default App;
